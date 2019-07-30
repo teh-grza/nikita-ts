8aaa304f8b7eea70e65e8c1c12c0ed322a4c767d
Nikita Neupane <nikitaneupane@Nikitas-MacBook-Pro.local>
2019-07-30 16:16:14 -0700

    navigation changes

---
+++ web/wp-content/themes/twentyseventeen/flexible-content.php
@@ -0,0 +1,4 @@
+<?php
+if ( get_row_layout() === 'two_three_column' ) :
+    include get_template_directory() . '/template-parts/components/2-or-3up-cta.php';
+endif;
--- web/wp-content/themes/twentyseventeen/template-parts/3-up_cta.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-echo "<img src='desktop/lala.jpg' alt='photo of the honest company' />";
-echo "<img src='desktop/didi.jpg' alt='nature' />";
-
-?>
